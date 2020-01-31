22571435c280154a9e868fc2361b558ceb5ff4ec
Yuriy Markov <peacefullatom@gmail.com>
2020-01-31 09:39:50 +0500

    added SEO

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,37 +1,45 @@
-import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
+import React from 'react';
 
-import {Layout} from '../components/index';
-import {htmlToReact, safePrefix} from '../utils';
+import { Layout } from '../components';
+import SEO from '../components/SEO';
+import { htmlToReact, safePrefix } from '../utils';
 
 export default class Post extends React.Component {
-    render() {
-        return (
-            <Layout {...this.props}>
-              <article className="post post-full">
-                <header className="post-header">
-                  <h1 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                </header>
-                {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
-                <div className="post-subtitle">
-                  {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
-                </div>
-                }
-                {_.get(this.props, 'pageContext.frontmatter.content_img_path') && 
-                <div className="post-thumbnail">
-                  <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.content_img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
-                </div>
-                }
-                <div className="post-content">
-                  {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                </div>
-                <footer className="post-meta">
-                  <time className="published"
-                    dateTime={moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%A, %B %e, %Y')}</time>
-                </footer>
-              </article>
-            </Layout>
-        );
-    }
+  render() {
+    const title = _.get(this.props, 'pageContext.frontmatter.title');
+    const image = _.get(this.props, 'pageContext.frontmatter.content_img_path');
+    const subtitle = _.get(this.props, 'pageContext.frontmatter.subtitle');
+    const safeImage = safePrefix(image);
+    const date = moment(_.get(this.props, 'pageContext.frontmatter.date'));
+    return (
+      <Layout {...this.props}>
+        <SEO
+          title={title}
+          image={safeImage}
+          description={_.get(this.props, 'pageContext.excerpt')}
+          pathname={_.get(this.props, 'pageContext.canonical_url')}
+          article
+        ></SEO>
+        <article className="post post-full">
+          <header className="post-header">
+            <h1 className="post-title underline">{title}</h1>
+          </header>
+          {subtitle && <div className="post-subtitle">{htmlToReact(subtitle)}</div>}
+          {image && (
+            <div className="post-thumbnail">
+              <img src={safeImage} alt={title} />
+            </div>
+          )}
+          <div className="post-content">{htmlToReact(_.get(this.props, 'pageContext.html'))}</div>
+          <footer className="post-meta">
+            <time className="published" dateTime={date.strftime('%Y-%m-%d %H:%M')}>
+              {date.strftime('%A, %B %e, %Y')}
+            </time>
+          </footer>
+        </article>
+      </Layout>
+    );
+  }
 }
